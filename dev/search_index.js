5914dbf1c86f25a9fba82bfbd7fed5b527feeff2
Documenter.jl <documenter@juliadocs.github.io>
2022-02-24 21:48:18 +0000

    build based on b138760

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DirectTranscription","category":"page"},{"location":"#DirectTranscription","page":"Home","title":"DirectTranscription","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DirectTranscription.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DirectTranscription]","category":"page"}]
+}
